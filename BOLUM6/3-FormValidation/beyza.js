3c2e68febecb41921c81f3c17111b2837fb7c757
Beyza Hız <beyzahiz.03@gmail.com>
2025-03-13 01:33:09 +0300

    Form Validation

---
+++ BOLUM6/3-FormValidation/beyza.js
@@ -0,0 +1,18 @@
+const form=document.querySelector('.signupForm');
+const message=document.querySelector('.message');
+
+form.addEventListener('submit', e=>{
+    e.preventDefault();
+    const username=form.username.value;
+    const usernamePattern=/^[a-z]{6,10}$/;
+
+    if(usernamePattern.test(username))
+    {
+        message.textContent='Başarılı';
+    }
+    else
+    {
+        message.textContent='Lütfen uygun hale getirin'
+    }
+
+})
