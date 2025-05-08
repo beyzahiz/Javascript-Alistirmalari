b102ce42587260e0e72c48d3191c50c736714cd0
Beyza Hız <beyzahiz.03@gmail.com>
2025-05-08 20:15:11 +0300

    push pop methods

---
+++ BOLUM7/5-PushPopMethodu/beyza.js
@@ -0,0 +1,8 @@
+const points = [70,75,25,35,10,80,27];
+points.push(90);
+console.log(points);
+// yeni eleman olarak en sona 90'ı ekler
+
+points.pop();
+console.log(points);
+// en son elemanı siler, çıkartır
